657cffe3b6b8e99bbaf266c0abe92661aecd3040
Akmal-M <akmal3017445@gmail.com>
2021-05-06 14:02:22 +0500

    actions done

--- pages/_document.js
+++ pages/_document.js
@@ -1,22 +1,22 @@
-import Document, {Html, Head, Main, NextScript} from "next/document";
+import Document, {Html, Head, Main, NextScript} from 'next/document'
 
-class MyDocument extends Document {
-    render() {
-        return (
-            <Html lang='en'>
+class MyDocument extends Document{
+    render(){
+        return(
+            <Html lang="en">
                 <Head>
-                    <meta name='description' content="Akmal's E-commerce website Next.js"/>
+                    <meta name="description" content="Dev AT E-commerce website with Next.js"/>
                     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" />
                     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
                     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
                 </Head>
                 <body>
-                <Main/>
-                <NextScript/>
+                <Main />
+                <NextScript />
                 </body>
             </Html>
         )
     }
 }
 
-export default MyDocument;
+export default MyDocument
