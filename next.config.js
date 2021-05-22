9ca52e74ffc1a2c28a0922e5407392be391083d3
Akmal-M <akmal3017445@gmail.com>
2021-05-22 09:09:08 +0500

    last

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     env: {
-        "BASE_URL" : "http://localhost:3000",
+        "BASE_URL" : "https://e-commerce-nextjs-akmalento-bbydyorfe-akmal-m.vercel.app/",
         "MONGODB_URL":"mongodb+srv://Akmal:565462123@cluster0.szrv0.mongodb.net/nextjs_ecommerce?retryWrites=true&w=majority",
         "ACCESS_TOKEN_SECRET" : "Qy/A>D<?+4C{gtHP^dL8_%yWT>j_jBsck7K5=4#e{<JSP)$CMX",
         "REFRESH_TOKEN_SECRET" : "y{ZnKEM@-a&jdrbAENr![F.Y7U-JU8!wqmWS.\\.8rc'UTm+_]fn$d^qUVW+35H'$'7!Ase==G*f7HEK?",
